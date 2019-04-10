d68a0e80ccf7a0e2e8ee529a1ba1364b82913fcb
Pete Bekos <peterbekos@gmail.com>
2019-04-09 19:48:33 -0500

    add test unit test

---
+++ XamarinSolution/TextContentAnalysis/Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using TextContentAnalysis;
+using Xunit;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            TextContentAnalyzer textContentAnalyzer = new TextContentAnalyzer();
+            var results = textContentAnalyzer.GetWordFrequency("one, two, two, three, three, three");
+            Assert.Equal(1, results["one"]);
+            Assert.Equal(2, results["two"]);
+            Assert.Equal(3, results["three"]);
+        }
+    }
+}
